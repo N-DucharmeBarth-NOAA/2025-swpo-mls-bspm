e17fec220b92dfd51d1189c379df30134b0a2d9b
N-DucharmeBarth-NOAA <nicholas.ducharme-barth@noaa.gov>
2025-06-24 11:42:05 -1000

    sensitivities to new model set-up

---
+++ code/R/04-07-run-bspm-effort-sens.r
@@ -0,0 +1,298 @@
+# Nicholas Ducharme-Barth
+# 2025/06/19
+# Run BSPM with effort-based fishing mortality and updated prior structure
+
+# Copyright (c) 2025 Nicholas Ducharme-Barth
+# You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# load packages
+    library(data.table)
+    library(magrittr)
+    library(rstan)
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# define directories
+    proj_dir = this.path::this.proj()
+    dir_helper_fns = file.path(proj_dir,"code","R","helper-fns")
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# source helper functions
+    sapply(file.path(dir_helper_fns,(list.files(dir_helper_fns))),source)    
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# make directory for model outputs
+    dir.create(file.path(proj_dir,"data","output","model_runs"), showWarnings = FALSE, recursive = TRUE)
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# load inputs
+    catch_dt = fread(file.path(proj_dir,"data","input","catch.csv"))
+    cpue_dt = fread(file.path(proj_dir,"data","input","cpue.csv"))
+    
+    # Load effort data 
+    effort_dt = fread(file.path(proj_dir, "data", "input", "WCPFC_L_PUBLIC_BY_FLAG_YR.CSV")) %>%
+                .[, lat_short_d := ifelse(grepl("S$", lat_short), 
+                                        -as.numeric(gsub("[NS]", "", lat_short)), 
+                                        as.numeric(gsub("[NS]", "", lat_short))) + 2.5] %>%
+                .[, lon_short_d := ifelse(grepl("W$", lon_short), 
+                                        360 - as.numeric(gsub("[EW]", "", lon_short)), 
+                                            as.numeric(gsub("[EW]", "", lon_short))) + 2.5] %>%
+                .[lat_short_d > -60 & lat_short_d < 0 & lon_short_d < 230] %>%
+                .[!(lat_short_d > -5 & lon_short_d > 210)] %>%
+                .[,.(effort_scaled = sum(hhooks)/1e6),by=yy] %>%
+                .[yy %in% 1952:2022] %>%
+                setnames(., "yy", "time")
+    
+    # Load updated prior parameters from pushforward analysis
+    mv_mean_catch = read.csv(file.path(proj_dir,"data","output","pushforward","q","mv_mean_catch.csv"))$x
+    mv_cov_catch = as.matrix(read.csv(file.path(proj_dir,"data","output","pushforward","q","mv_cov_catch.csv"))[,-1])
+    mv_cor_catch = as.matrix(read.csv(file.path(proj_dir,"data","output","pushforward","q","mv_cor_catch.csv"))[,-1])
+    
+    # Independent qeff prior
+    qeff_pars_catch = read.csv(file.path(proj_dir,"data","output","pushforward","q","qeff_pars_catch.csv"))$x
+    
+    # Bivariate rho/sigma_qdev prior
+    mv_qdev_mean = read.csv(file.path(proj_dir,"data","output","pushforward","q","mv_qdev_mean.csv"))$x
+    mv_qdev_cov = as.matrix(read.csv(file.path(proj_dir,"data","output","pushforward","q","mv_qdev_cov.csv"))[,-1])
+    mv_qdev_cor = as.matrix(read.csv(file.path(proj_dir,"data","output","pushforward","q","mv_qdev_cor.csv"))[,-1])
+    
+    # Other priors
+    dep_pars = read.csv(file.path(proj_dir,"data","output","rel_dep_ssb_pars.csv"))
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# load additional cpue indices
+    au_cpue_dt = fread(file.path(proj_dir,"data","input","AU_cpue.csv"))
+    nz_cpue_dt = fread(file.path(proj_dir,"data","input","NZ_cpue.csv"))
+    obs_cpue_dt = fread(file.path(proj_dir,"data","input","obs-idx.csv"))
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# prepare data matrices
+    catch_annual = catch_dt[,.(total_catch = sum(Obs * 1000)), by = .(year = floor(Time))]
+    setorder(catch_annual, year)
+    
+    # Merge with effort data
+    catch_effort_annual = merge(catch_annual, effort_dt, by.x = "year", by.y = "time", all.x = TRUE)
+    
+    # Fill missing effort with mean (if any)
+    if(any(is.na(catch_effort_annual$effort_scaled))) {
+        catch_effort_annual[is.na(effort_scaled), effort_scaled := mean(effort_scaled, na.rm = TRUE)]
+    }
+    
+    time_years = catch_effort_annual$year
+    n_years = length(time_years)
+    
+    index_mat = matrix(-999, nrow = n_years, ncol = 4)
+    se_mat = matrix(-999, nrow = n_years, ncol = 4)
+    mean_se = rep(NA,4)
+
+    # dwfn cpue
+    cpue_years = floor(cpue_dt$Time)
+    mean_se[1] = mean(cpue_dt$SE,na.rm=TRUE)
+    for(i in 1:nrow(cpue_dt)) {
+        year_idx = which(time_years == cpue_years[i])
+        if(length(year_idx) > 0) {
+            index_mat[year_idx, 1] = cpue_dt$Obs[i]
+            se_mat[year_idx, 1] = cpue_dt$SE[i]/mean_se[1]
+        }
+    }
+
+    # australia cpue
+    mean_se[2] = mean(au_cpue_dt$se_log,na.rm=TRUE)
+    for(i in 1:nrow(au_cpue_dt)) {
+        year_idx = which(time_years == au_cpue_dt$year[i])
+        if(length(year_idx) > 0) {
+            index_mat[year_idx, 2] = au_cpue_dt$obs[i]/mean(au_cpue_dt$obs)
+            se_mat[year_idx, 2] = au_cpue_dt$se_log[i]/mean_se[2]
+        }
+    }
+
+    # new zealand cpue
+    mean_se[3] = mean(nz_cpue_dt$se_log,na.rm=TRUE)
+    for(i in 1:nrow(nz_cpue_dt)) {
+        year_idx = which(time_years == nz_cpue_dt$year[i])
+        if(length(year_idx) > 0) {
+            index_mat[year_idx, 3] = nz_cpue_dt$ob[i]/mean(nz_cpue_dt$ob)
+            se_mat[year_idx, 3] = nz_cpue_dt$se_log[i]/mean_se[3]
+        }
+    }
+
+    # Observer CPUE
+    # calc SE from quantiles
+    obs_cpue_dt[, se_from_quantiles := (Q97.5 - Q2.5) / (2 * 1.96)]
+    mean_se[4] = mean(obs_cpue_dt$se_from_quantiles,na.rm=TRUE)
+    for(i in 1:nrow(obs_cpue_dt)) {
+        year_idx = which(time_years == obs_cpue_dt$Year[i])
+        if(length(year_idx) > 0) {
+            index_mat[year_idx, 4] = obs_cpue_dt$Estimate[i]/mean(obs_cpue_dt$Estimate)
+            se_mat[year_idx, 4] = obs_cpue_dt$se_from_quantiles[i]/mean_se[4]
+        }
+    }
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# prepare multivariate prior parameters
+    # Extract standard deviations from covariance matrix (3D now)
+    mv_prior_sd = sqrt(diag(mv_cov_catch))
+    
+    # Extract standard deviations for qdev parameters (2D)
+    mv_qdev_prior_sd = sqrt(diag(mv_qdev_cov))
+
+    # Validate correlation matrices are positive definite
+    if(min(eigen(mv_cor_catch)$values) < 1e-10) {
+        warning("3D correlation matrix has very small eigenvalues, adding regularization")
+        mv_cor_catch = mv_cor_catch + diag(1e-8, 3)
+    }
+    
+    if(min(eigen(mv_qdev_cor)$values) < 1e-10) {
+        warning("2D correlation matrix has very small eigenvalues, adding regularization")  
+        mv_qdev_cor = mv_qdev_cor + diag(1e-8, 2)
+    }
+    
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# compile executable
+    exec_name = "bspm_estq_softdep_mvprior" # bspm_estq_softdep_mvprior # bspm_estq_optimized
+    stan_c = stan_model(file=file.path(proj_dir,"code","Stan",paste0(exec_name,".stan")), model_name = exec_name)
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# develop model grid
+    model_config_df = rbind( expand.grid(cpue=c("au","nz","obs"),
+                                  sigma_catch = c(0.2),
+                                  sigma_edev = c(0.3),
+                                  n_step=5),
+                            expand.grid(cpue=c("dwfn"),
+                                  sigma_catch = c(0.1,0.2,0.4),
+                                  sigma_edev = c(0.3,0.5),
+                                  n_step=c(2,3,4,5)),
+                            expand.grid(cpue=c("dwfn"),
+                                  sigma_catch = c(0.1),
+                                  sigma_edev = c(0.3,0.4,0.5),
+                                  n_step=5)
+    )
+
+    model_config_df = unique(model_config_df)
+
+#________________________________________________________________________________________________________________________________________________________________________________________________________
+# set-up model inputs
+
+for(i in 1:nrow(model_config_df)){
+        run_label_stem = paste0(model_config_df$cpue[i],"-c",model_config_df$sigma_catch[i],"-e",model_config_df$sigma_edev[i],"-s",model_config_df$n_step[i],"_0")
+        run_number = 6 + i
+        if(run_number<10){
+            run_number = paste0("000",run_number)
+        } else {
+            run_number = paste0("00",run_number)
+        }
+
+        if(model_config_df$cpue[i]=="dwfn"){
+            lambda_vec = c(1,0,0,0)
+            sigmao_input = mean_se[1]
+        } else if(model_config_df$cpue[i]=="au"){
+            lambda_vec = c(0,1,0,0)
+            sigmao_input = mean_se[2]
+        } else if(model_config_df$cpue[i]=="nz"){
+            lambda_vec = c(0,0,1,0)
+            sigmao_input = mean_se[3]
+        } else {
+            lambda_vec = c(0,0,0,1)
+            sigmao_input = mean_se[4]
+        }
+
+ # Define effort parameters
+    n_step = model_config_df$n_step[i]  # years per period for catchability
+    n_periods = ceiling((n_years-1) / n_step)
+            
+    # specify data and priors
+    tmp.data.priors = list(
+        # 3D multivariate prior (logK, log_r, log_shape)
+        mv_prior_mean = mv_mean_catch,
+        mv_prior_sd = mv_prior_sd,
+        mv_prior_corr = mv_cor_catch,
+        
+        # Independent qeff prior
+        prior_qeff_meanlog = qeff_pars_catch[1],
+        prior_qeff_sdlog = qeff_pars_catch[2],
+        
+        # Bivariate rho/sigma_qdev prior  
+        mv_qdev_prior_mean = mv_qdev_mean,
+        mv_qdev_prior_sd = mv_qdev_prior_sd,
+        mv_qdev_prior_corr = mv_qdev_cor,
+        
+        # Other priors
+        PriorMean_logsigmap = -2.9311037,
+        PriorSD_logsigmap = 0.2661089,
+        PriorSD_sigmao_add = 0.2,
+        prior_depletion_meanlog = dep_pars$x[1],
+        prior_depletion_sdlog = dep_pars$x[2]
+    )
+
+    tmp_data = list(
+        T = as.integer(n_years),
+        I = as.integer(ncol(index_mat)),
+        index = index_mat,
+        sigmao_mat = se_mat,
+        lambdas = lambda_vec,
+        t_dep = 37, # model starts t=1 in 1952, 37 is 1988
+        use_depletion_prior = 0L, # Set to 1L to use prior, 0L to skip it
+        fit_to_data = 1L, # Set to 1L to fit to data, 0L to skip likelihoods
+        epsilon = 1e-10,
+        
+        # Effort-based parameters
+        effort = catch_effort_annual$effort_scaled,
+        n_step = as.integer(n_step),
+        n_periods = as.integer(n_periods),
+        sigma_edev = model_config_df$sigma_edev[i],  # Prior SD for effort deviations
+        
+        # Observation error parameters
+        sigmao_input = sigmao_input,
+        obs_removals = catch_effort_annual$total_catch,
+        sigmac = model_config_df$sigma_catch[i]
+    )
+                     
+    stan.data = c(tmp_data, tmp.data.priors)
+    # stan.data.ppc = stan.data
+    # stan.data.ppc$fit_to_data = 0L
+
+    # ppc = fit_rstan(stan.data.ppc,
+    #                 stan_c,
+    #                 run_label = paste0(run_number,"-",run_label_stem,"-ppc"),
+    #                 exec_name = exec_name,
+    #                 seed = 321,
+    #                 chains = 5,
+    #                 n_thin = 10,
+    #                 iter_keep = 1000,
+    #                 burnin.prop = 0.5,
+    #                 adapt_delta = 0.99,
+    #                 max_treedepth = 12,
+    #                 silent = FALSE,
+    #                 stan_save_dir = file.path(proj_dir,"data","output","model_runs"),
+    #                 n_cores = 5)
+
+    fit = fit_rstan(stan.data,
+                    stan_c,
+                    run_label = paste0(run_number,"-",run_label_stem),
+                    exec_name = exec_name,
+                    seed = 321,
+                    chains = 5,
+                    n_thin = 10,
+                    iter_keep = 200,
+                    burnin.prop = 0.5,
+                    adapt_delta = 0.99,
+                    max_treedepth = 12,
+                    silent = FALSE,
+                    stan_save_dir = file.path(proj_dir,"data","output","model_runs"),
+                    n_cores = 5)
+
+    print(fit, pars = c("logK", "r", "shape", "sigmap", "sigmao_add", "qeff", "rho", "sigma_qdev", 
+                       "x[1]", "x[37]", "x[71]", "removals[3]", "removals[70]"))
+    print(stan.data$obs_removals[c(3,70)])
+    
+    quick_diagnostics(fit)
+    # compare_marginals(ppc,fit,c("logK", "r", "shape", "sigmap", "sigmao_add", "qeff", "rho", "sigma_qdev", 
+    #                    "x[1]", "x[37]", "x[71]", "removals[3]", "removals[70]"))
+
+    t=as.data.table(summary(fit)$summary)
+    t$name = rownames(summary(fit)$summary)
+    t[n_eff<500|Rhat>1.01]
+}
+    
+    
+   
---
+++ code/Stan/bspm_estq_flex.stan
@@ -0,0 +1,289 @@
+// Modified fletcher-schaefer surplus production model with effort-based fishing mortality
+// Updated prior structure based on pushforward analysis:
+// - 3D multivariate prior for logK, log_r, log_shape  
+// - Independent qeff parameter
+// - Correlated rho and sigma_qdev parameters
+// - more flexible qdev and sigmac[T]
+
+data {
+    int T; // time dimension
+    int I; // number of indices
+    real index[T,I]; // matrix of indices
+    real obs_removals[T]; // time series of catch
+    real sigmao_mat[T,I]; // observation error corresponding to each index (scaled to mean 1)
+    real sigmao_input; // observation error
+    int lambdas[I]; // lambdas for CPUE
+    real sigmac[T]; // observation error for catch
+    real sigma_edev; // effort dev variation
+    int t_dep; // year of depletion prior
+    int use_depletion_prior; // boolean flag (0/1) to control depletion prior
+    int fit_to_data; // boolean flag (0/1) to control if likelihoods are turned on
+
+    // New effort-based parameters  
+    real effort[T]; // effort time series
+    int n_periods; // number of catchability periods
+    int<lower=1, upper=n_periods> q_period_index[T]; // used to match qdevs to the right time step, must be monotonically non-decreasing integers no greater than n_periods, timestep Tm1 must equal n_periods
+
+    // Updated multivariate priors (now 3-dimensional: logK, log_r, log_shape)
+    vector[3] mv_prior_mean; // mean vector [logK, log_r, log_shape]
+    vector[3] mv_prior_sd; // standard deviations 
+    corr_matrix[3] mv_prior_corr; // correlation matrix
+
+    // Independent qeff prior parameters
+    real prior_qeff_meanlog; // mean on log scale
+    real prior_qeff_sdlog; // SD on log scale
+
+    // Bivariate prior for rho and sigma_qdev (on transformed scale)
+    vector[2] mv_qdev_prior_mean; // [atanh_rho_mean, log_sigma_qdev_mean]
+    vector[2] mv_qdev_prior_sd; // standard deviations
+    corr_matrix[2] mv_qdev_prior_corr; // correlation matrix
+
+    // Other priors
+    real PriorMean_logsigmap;
+    real PriorSD_logsigmap;
+    real PriorSD_sigmao_add;
+    real prior_depletion_meanlog; // prior mean on log scale
+    real prior_depletion_sdlog; // prior SD on log scale
+}
+
+transformed data{
+    int Tm1;
+    Tm1 = T-1;
+
+    // Validate q_period_index meets all requirements
+    // 1. Bounds check
+    for(t in 1:T) {
+        if(q_period_index[t] < 1 || q_period_index[t] > n_periods) {
+            reject("q_period_index[", t, "] = ", q_period_index[t], 
+                   " violates bounds [1, ", n_periods, "]");
+        }
+    }
+    
+    // 2. Monotonically non-decreasing check
+    for(t in 2:T) {
+        if(q_period_index[t] < q_period_index[t-1]) {
+            reject("q_period_index must be monotonically non-decreasing. ",
+                   "Position ", t, " value ", q_period_index[t], 
+                   " < position ", t-1, " value ", q_period_index[t-1]);
+        }
+    }
+    
+    // 3. Final timestep must equal n_periods
+    if(q_period_index[Tm1] != n_periods) {
+        reject("q_period_index[Tm1=", Tm1, "] = ", q_period_index[Tm1], 
+               " must equal n_periods = ", n_periods);
+    }
+}
+
+parameters {
+    // Updated multivariate parameters (now 3-dimensional)
+    vector[3] raw_mv_params; // [raw_logK, raw_log_r, raw_log_shape]
+
+    // Independent qeff parameter
+    real raw_logqeff; // raw log effort catchability
+
+    // Bivariate parameters for rho and sigma_qdev
+    vector[2] raw_qdev_params; // [raw_atanh_rho, raw_log_sigma_qdev]
+
+    real raw_logsigmap;
+    real<lower=0> raw_sigmao_add;
+    real raw_epsp[T];
+    
+    // Effort-based parameters
+    vector[n_periods] raw_qdev_period; // raw period-specific deviations (non-centered)
+    vector[Tm1] raw_edev; // raw annual effort deviations (non-centered)
+}
+
+transformed parameters {
+    // leading parameters
+    real logK;
+    real r;
+    real shape;
+    real logqeff; // effort-based catchability (independent)
+    real qeff; // transformed effort catchability
+    real dev[T]; // recruitment deviates
+    real epsp[T]; // process error (multiplicative)
+    real sigmap;
+    real rho; // AR correlation for catchability
+    real sigma_qdev; // catchability variability
+
+    // For backward compatibility - extract individual raw parameters
+    real raw_logK;
+    real raw_logr;
+    real raw_logshape;
+    real raw_rho;
+    real raw_sigma_qdev;
+
+    // Updated multivariate transformation (3-dimensional)
+    vector[3] mv_params;
+    mv_params = mv_prior_mean + diag_pre_multiply(mv_prior_sd, cholesky_decompose(mv_prior_corr)) * raw_mv_params;
+    
+    // Extract individual parameters (transformed scale)
+    logK = mv_params[1]; // logK (already on log scale)
+    r = exp(mv_params[2]); // r (transform from log scale)
+    shape = exp(mv_params[3]); // shape (transform from log scale)
+    
+    // Extract individual raw parameters (for compatibility)
+    raw_logK = (mv_params[1] - mv_prior_mean[1]) / mv_prior_sd[1];
+    raw_logr = (mv_params[2] - mv_prior_mean[2]) / mv_prior_sd[2];
+    raw_logshape = (mv_params[3] - mv_prior_mean[3]) / mv_prior_sd[3];
+    
+    // Independent qeff parameter
+    logqeff = raw_logqeff * prior_qeff_sdlog + prior_qeff_meanlog;
+    qeff = exp(logqeff);
+    
+    // Bivariate transformation for rho and sigma_qdev
+    vector[2] qdev_params;
+    qdev_params = mv_qdev_prior_mean + diag_pre_multiply(mv_qdev_prior_sd, cholesky_decompose(mv_qdev_prior_corr)) * raw_qdev_params;
+    
+    // Transform parameters back to natural scale
+    rho = tanh(qdev_params[1]); // atanh_rho -> rho
+    sigma_qdev = exp(qdev_params[2]); // log_sigma_qdev -> sigma_qdev
+    
+    // Extract individual raw parameters for rho and sigma_qdev (for compatibility)
+    raw_rho = (qdev_params[1] - mv_qdev_prior_mean[1]) / mv_qdev_prior_sd[1];
+    raw_sigma_qdev = (qdev_params[2] - mv_qdev_prior_mean[2]) / mv_qdev_prior_sd[2];
+    
+    // Effort-based fishing mortality calculation with dual error structure
+    vector[n_periods] qdev_period; // transformed period deviations
+    real qdev[Tm1]; // systematic catchability changes
+    real edev[Tm1]; // effort measurement errors
+    real F[Tm1];
+    
+    // Non-centered parameterization for AR(1) catchability process
+    qdev_period[1] = raw_qdev_period[1] * sigma_qdev;
+    for(p in 2:n_periods) {
+        qdev_period[p] = rho * qdev_period[p-1] + raw_qdev_period[p] * sigma_qdev * sqrt(1 - rho^2);
+    }
+    
+    // Assign period-based qdev with temporal structure
+    for(t in 1:Tm1) {
+        qdev[t] = qdev_period[q_period_index[t]];
+    }
+    
+    // Independent annual effort deviations (non-centered)
+    for(t in 1:Tm1) {
+        edev[t] = raw_edev[t] * sigma_edev;
+    }
+    
+    // Calculate fishing mortality: F_t = qeff * exp(qdev_t) * effort_t * exp(edev_t - sigma_edev^2/2)
+    for(t in 1:Tm1) {
+        F[t] = qeff * exp(qdev[t]) * effort[t] * exp(edev[t] - sigma_edev^2/2);
+    }
+
+    // process error
+    sigmap = exp(raw_logsigmap*PriorSD_logsigmap + PriorMean_logsigmap); // lognormal prior
+    real sigmap2;
+    sigmap2 = pow(sigmap,2);
+
+    // observation error
+    real sigmao[T,I];
+    real sigmao_sc;
+    real sigmao_add;
+    sigmao_add = raw_sigmao_add*PriorSD_sigmao_add;
+    sigmao_sc = sigmao_input + sigmao_add;
+    for(t in 1:T){
+        dev[t] = raw_epsp[t]*sigmap;
+        epsp[t] = exp(dev[t]-sigmap2/2);
+        for(i in 1:I){
+            sigmao[t,i] = sigmao_mat[t,i] * sigmao_sc;
+        }
+    }
+    
+    // fletcher-schaefer
+    // parameters
+    real n;
+    real dmsy;
+    real h;
+    real m;
+    real g;
+
+    n = shape;
+    dmsy = pow((1/n),(1/(n-1)));
+    h = 2*dmsy;
+    m = r*h/4;
+    g = pow(n,(n/(n-1)))/(n-1);
+
+    // population dynamics
+    real removals[Tm1]; // catch (relative to logK) in each time step
+    real x[T]; // population time series relative to logK
+    x[1] = epsp[1];
+    for(t in 2:T) {
+        if(x[t-1]<=dmsy){
+            x[t] = ((x[t-1] + r * x[t-1] * (1 - x[t-1]/h)))*epsp[t]*(exp(-F[t-1]));
+            removals[t-1] = ((x[t-1] + r * x[t-1] * (1 - x[t-1]/h)))*epsp[t]*(1-exp(-F[t-1]))*exp(logK);
+        } 
+        if(x[t-1]> dmsy){
+            x[t] = ((x[t-1] + g * m * x[t-1] * (1 - pow(x[t-1],(n-1)))))*epsp[t]*(exp(-F[t-1]));
+            removals[t-1] = ((x[t-1] + g * m * x[t-1] * (1 - pow(x[t-1],(n-1)))))*epsp[t]*(1-exp(-F[t-1]))*exp(logK);
+        } 
+    }
+
+    // Analytical q calculation for observation model (KEPT SEPARATE)
+    real q[I]; // catchability for indices
+    real sigmao2[T,I]; // observation error for each index
+    real sum1;
+    real sum2;
+    real p;
+    for(i in 1:I){
+        sum1 = 0.0;
+        sum2 = 0.0;
+        p = 0.0;
+        for(t in 1:T){
+            sigmao2[t,i] = square(sigmao[t,i]);
+            if(index[t,i]>0.0 && x[t]>0.0) {
+                sum1 = sum1 + log(index[t,i]/x[t])/sigmao2[t,i];
+                sum2 = sum2 + 1/sigmao2[t,i];
+                p = p + 1.0;
+            }
+        }
+        if(p>2.0){
+            q[i] = exp((0.5 * p + sum1) / sum2);
+        } else{
+            q[i] = 0.0;
+        } 
+    }
+} 
+
+model {
+    // Updated multivariate normal prior (3-dimensional)
+    raw_mv_params ~ std_normal(); // Standard normal for raw parameters
+
+    // Independent qeff prior (lognormal)
+    raw_logqeff ~ std_normal(); // Standard normal for raw parameter
+
+    // Bivariate prior for rho and sigma_qdev
+    raw_qdev_params ~ std_normal(); // Standard normal for raw parameters
+    
+    // Effort-based priors (non-centered)
+    raw_qdev_period ~ std_normal(); // All catchability deviations are standard normal
+    raw_edev ~ std_normal(); // All effort deviations are standard normal
+    
+    // prior densities for other estimated parameters
+    raw_epsp ~ std_normal();
+    raw_logsigmap ~ std_normal();
+    raw_sigmao_add ~ std_normal();
+    
+    // Lognormal prior depletion prior from Gedamke and Hoenig 2006 size based Z estimate
+    if(use_depletion_prior == 1) {
+        target += lognormal_lpdf(x[t_dep] | prior_depletion_meanlog, prior_depletion_sdlog);
+    }
+    
+    // observation model - uses analytical q[i]
+    if(fit_to_data == 1) {
+        for(i in 1:I){
+            if(lambdas[i]==1){
+                for(t in 1:T){
+                    if(index[t,i]>0.0 && x[t]>0.0 && q[i]>0.0) {
+                        target += lognormal_lpdf(index[t,i] | log(q[i]*x[t]) - sigmao2[t,i]/2,sigmao[t,i]);
+                    }
+                }
+            }
+        }
+
+        // Catch observation model uses F derived from effort-based qeff
+        for(t in 1:Tm1){
+            target += lognormal_lpdf(obs_removals[t]|log(removals[t]) - 0.5*sigmac[t]^2,sigmac[t]);
+        }
+    }
+}
